c28bc86cbf65c835f49d385d754d00144d03239f
YUTA KATSUBE <herojejung@users.noreply.github.com>
2022-10-20 23:13:55 +0000

    [update]bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,13 +4,18 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+  if @book.save
+    redirect_to book_path(@book.id), notice: 'Book was successfully created.'
+  else
+    @books = Book.all
+    render :index
+  end
   end
 
   def index
      @books = Book.all
+     @book = Book.new
   end
 
   def show
@@ -22,9 +27,18 @@ class BooksController < ApplicationController
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
+    else
+    render :edit
+    end
+  end
+
+  def destroy
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    book.destroy
+    redirect_to '/books'
   end
 
   private
@@ -32,5 +46,4 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-
 end
